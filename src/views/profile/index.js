4f11a7517cfbebda12488a70452fc2e51f03be34
sanaz_javadi <sanazjavadi72@yahoo.com>
2021-02-20 15:59:37 +0330

    profile component

--- src/views/profile/index.js
+++ src/views/profile/index.js
@@ -2,21 +2,41 @@ import React from 'react'
 
 //component
 import Hero from '../../layouts/common/Hero'
+import Button from '../../components/Button'
 
 //styles
 import styles from './styles/Profile.module.scss'
 
+//assets
+import ProfileSvg from '../../svg/Profile'
+
 function Profile(props) {
-    
+
 
     return (
         <div>
-            <Hero/>
-             <section className={styles.profile}>
-          <div className={styles.userInfo}>
- dddd
-          </div>
-             </section>
+            <Hero />
+            <section className={styles.profile}>
+                <div className={`d-flex align-items-center justify-content-between px-5 ${styles.user}`}>
+                    <div className={`${styles.userInfo} d-flex`}>
+                    <span className={styles.profileImg}>
+                        <ProfileSvg fill="white" width="25" height="25"/>
+                    </span>
+                    <div className={`${styles.userName} d-flex flex-column text-right mr-3`}>
+                        <span className="font-weight-bold">
+                        sanaz javadi
+                        </span>
+                        <span>
+                            sanazjavadi@gmail.com
+                        </span>
+                    </div>
+                    </div>
+                  
+                    <Button className="mr-auto">
+                        ویرایش اطلاعات کاربری
+                    </Button>
+                </div>
+            </section>
         </div>
     )
 }
