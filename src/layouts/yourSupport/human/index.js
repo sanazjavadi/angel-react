e05f96e8336d6787bdc23159455a3f799d8227e4
sanaz_javadi <sanazjavadi72@yahoo.com>
2021-03-01 17:07:54 +0330

    responsive <support/>

--- src/layouts/yourSupport/human/index.js
+++ src/layouts/yourSupport/human/index.js
@@ -10,18 +10,18 @@ function HumanHelp(props) {
 
     return (
         <section>
-            <div className={` my-5 ${styles.header}`}>
-                <h3 className={`text-center`}>راه های کمک</h3>
+            <div className={` my-lg-5 my-3 ${styles.header}`}>
+                <h3 className={`text-center mb-2 pb-0`}>راه های کمک</h3>
                 <p className="text-center">شما عزیزان به عنوان فرد میتوانید از راه های زیر به کودکان کمک کنی</p>
             </div>
-            <Row className="justify-content-strech align-items-center">
+            <Row className="justify-content-between align-items-center">
 
                 <Col lg="6" className={`text-right mt-4 ${styles.rightContent}`}>
                     <h4>اهدای کمک های غیرنقدی</h4>
                     <p>خیرین محترم می­‌توانند با مراجعه به خیریه هدایای غیرنقدی و اجناس اهدایی خود را شامل: لوازم التحریر، پوشاک، خواروبار و مواد بهداشتی، مواد شوینده، میوه و سبزیجات، مواد پروتئینی و لبنی و غیره را دراختیار مسئولین قرار دهند تا این هدایا به دست کودکان یا خانواده‌­های نیازمند رسانده شود در اینگونه موارد بهتر است از قبل با خیریه هماهنگی لازم به عمل آید.
                      </p>
                 </Col>
-                <Col lg="6" >
+                <Col lg="6" sm={12}>
                     <div className={`${styles['nocash-help-section']} m-auto`} >
                         <Button>
                             میخوام کمک کنم
@@ -37,8 +37,8 @@ function HumanHelp(props) {
 
                 </Col>
             </Row>
-            <Row className="justify-content-strech align-items-center">
-                <Col lg="6">
+            <Row className="justify-content-between align-items-center">
+                <Col lg="6" md={{span:12, order:2}} sm={{order: 2}} xs={{order:2}}>
                     <div className={`${styles['cash-help-section']} m-auto`} >
                         <Button>
                             میخوام کمک کنم
@@ -53,7 +53,7 @@ function HumanHelp(props) {
 
                 </Col>
 
-                <Col lg="6" className={`text-right mt-4 ${styles.rightContent}`}>
+                <Col sm={{order: 1, span: 12}} md={{span:12, order:1}} xs={{order:1, span:12}} lg={{span:6 , order:2}} className={`text-right mt-4 ${styles.rightContent}`}>
                     <h4>اهدای کمک های نقدی</h4>
                     <p>
                         نیکوکاران محترم می‌­توانند کمک‌های نقدی خود را به شماره حساب 30200-2211 بانک تجارت شعبه برج آسمان واریز نموده و یا مستقیما به محل مؤسسه مراجعه و کمک‌های خود را تحویل داده و رسید دریافت نمایند.
--- src/views/yourSupport/index.js
+++ src/views/yourSupport/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import useWindowSize from '../../service/hooks/useWindowSize'
 
 //layouts
 import Hero from '../../layouts/common/Hero'
@@ -13,21 +14,22 @@ import { Tabs, Tab } from 'react-bootstrap'
 
 
 function YourSupport(props) {
-
-    const[key, setKey] = useState('human')
+    const size = useWindowSize()
+    console.log(size)
+    const [key, setKey] = useState('human')
     return (
         <div className="container-fluid">
             <Hero>
                 حمایت شما        </Hero>
             <section className={styles['section-margin']}>
                 <Tabs activeKey={key} onSelect={(k) => setKey(k)} defaultActiveKey="human" id="uncontrolled-tab-example" bsPrefix={styles['custom-tab']}>
-                  
-                    <Tab  eventKey="human" title="به عنوان فرد" style={{'width':' 65%'}}  className="mx-auto p-3">
-                       <Human/>
-                     </Tab>
-                     <Tab eventKey="org" title="به عنوان سازمان" style={{'width':' 65%'}}  className="mx-auto p-3" >
-                    <Organization/>
-                        </Tab>
+
+                    <Tab eventKey="human" title="به عنوان فرد" style={size > 600 ? { 'width': ' 65%' } : { width: '100%' }} className="mx-auto p-lg- p-2">
+                        <Human />
+                    </Tab>
+                    <Tab eventKey="org" title="به عنوان سازمان" style={size > 600 ? { 'width': ' 65%' } : { width: '100%' }} className="mx-auto p-lg-3 p-2" >
+                        <Organization />
+                    </Tab>
                 </Tabs>
             </section>
         </div>
